d6147f147cd501d367cd7a58a4e8dcbe5edf42a0
Wagner Reis <wagnerreis1997@gmail.com>
2019-11-06 13:44:11 -0300

    Excluir Tarefa(Task)

--- src/screens/Agenda.js
+++ src/screens/Agenda.js
@@ -66,6 +66,11 @@ export default class Agenda extends Component {
         this.setState({ tasks, showAddTask: false}, this.filterTasks)
     }
 
+    deleteTasks = id => {
+        const tasks = this.state.tasks.filter(task => task.id !== id)
+        this.setState({ tasks }, this.filterTasks)
+    }
+
     filterTasks = () => {
         let visibleTasks = null 
         if (this.state.showDoneTasks) {
@@ -122,7 +127,8 @@ export default class Agenda extends Component {
                     <FlatList data={this.state.visibleTasks}
                         keyExtractor={item => `${item.id}`}
                         renderItem={({ item }) =>
-                         <Task {...item} toggleTask={this.toggleTask} />} />
+                         <Task {...item} toggleTask={this.onToggleTask} 
+                            onDelete={this.deleteTasks} />} />
                 </View>
                 <ActionButton buttonColor={commonStyles.colors.today}
                     onPress={() => { this.setState({ showAddTask: true }) }} />
